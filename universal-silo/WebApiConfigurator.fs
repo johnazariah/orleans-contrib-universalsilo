06b3189e3acb45d317727721feebc33bf181e71a
John Azariah <john.azariah@gmail.com>
2020-04-20 21:23:25 -0700

    fix up the API to be more extensible

--- universal-silo/WebApiConfigurator.fs
+++ universal-silo/WebApiConfigurator.fs
@@ -11,9 +11,15 @@ open System.IO
 open System.IO.Compression
 open System.Reflection
 open System.Text.Json.Serialization
+open Microsoft.Extensions.Configuration
 
 [<AbstractClass>]
 type WebApiConfigurator (apiInfo : OpenApiInfo) = class
+    member val Configuration : IConfiguration = null with get, set
+
+    member this.SetConfigurationObject (hostBuilderContext : HostBuilderContext) =
+        this.Configuration <- hostBuilderContext.Configuration
+
     abstract AddSwaggerGen : IServiceCollection -> IServiceCollection
     default __.AddSwaggerGen services =
         let xmlFile = sprintf "%s.xml" (Assembly.GetEntryAssembly().GetName().Name)
@@ -31,8 +37,8 @@ type WebApiConfigurator (apiInfo : OpenApiInfo) = class
             .Configure(fun (options : GzipCompressionProviderOptions) ->
                 options.Level <- CompressionLevel.Optimal);
 
-    abstract ConfigureServices : IServiceCollection -> IServiceCollection
-    default this.ConfigureServices services =
+    abstract ConfigureServices : HostBuilderContext -> IServiceCollection -> IServiceCollection
+    default this.ConfigureServices hostBuilderContext services =
         services
         |> (fun sc -> sc.AddControllers())
         |> (fun mvc -> mvc.AddJsonOptions (fun options -> options.JsonSerializerOptions.Converters.Add (JsonStringEnumConverter()) ))
@@ -40,19 +46,19 @@ type WebApiConfigurator (apiInfo : OpenApiInfo) = class
 
         services
         |> this.AddSwaggerGen
-        //|> this.AddResponseCompression
+        |> this.AddResponseCompression
 
     abstract Configure : IApplicationBuilder -> IHostEnvironment -> unit
     default __.Configure appBuilder hostEnv =
         appBuilder
         |> (fun app -> if (not <| hostEnv.IsDevelopment ()) then app else app.UseDeveloperExceptionPage() |> ignore; app)
-        //|> (fun app -> app.UseDefaultFiles ())
-        //|> (fun app -> app.UseStaticFiles  ())
+        |> (fun app -> app.UseDefaultFiles ())
+        |> (fun app -> app.UseStaticFiles  ())
         |> (fun app -> app.UseHttpsRedirection    ())
         |> (fun app -> app.UseSwagger             ())
         |> (fun app -> app.UseSwaggerUI (fun options ->
             options.SwaggerEndpoint ("/swagger/v1/swagger.json", (sprintf "%s %s" apiInfo.Title apiInfo.Version))))
-        //|> (fun app -> app.UseResponseCompression ())
+        |> (fun app -> app.UseResponseCompression ())
         |> (fun app -> app.UseRouting             ())
         |> (fun app -> app.UseAuthorization       ())
         |> (fun app -> app.UseEndpoints (fun endpoints -> endpoints.MapControllers() |> ignore))
@@ -71,6 +77,7 @@ type WebApiConfigurator (apiInfo : OpenApiInfo) = class
     default this.ConfigureWebApiHost builder =
         builder
         |> (fun b -> b.ConfigureWebHostDefaults(fun _b -> this.ConfigureWebHost _b |> ignore))
-        |> (fun b -> b.ConfigureServices(fun (services : IServiceCollection) -> this.ConfigureServices services |> ignore))
+        |> (fun b -> b.ConfigureServices(fun hostBuilderContext _ -> this.SetConfigurationObject hostBuilderContext |> ignore))
+        |> (fun b -> b.ConfigureServices(fun hostBuilderContext services -> this.ConfigureServices hostBuilderContext services |> ignore))
 end
 
